d992cb5c9e698f26d919f6b9e0c56370a3b16d28
Alex <alexandr.yakimov.heaven@gmail.com>
2022-12-16 16:55:21 +0500

    Task is done

---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] matr = new int[4, 5];
+MatrixFillArray(matr);
+PrintMatrix(matr);
+System.Console.WriteLine("Упорядоченный массив: ");
+OrderingNumber(matr);
+PrintMatrix(matr);
+
+void OrderingNumber(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+
+}
+
+
+void MatrixFillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        System.Console.WriteLine();
+    }
+}
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] matr = new int[5, 4];
+MatrixFillArray(matr);
+PrintMatrix(matr);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(matr, 0);
+for (int i = 1; i < matr.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(matr, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+
+int[,] firstMartrix = new int[2, 2];
+MatrixFillArray(firstMartrix);
+Console.WriteLine("Первая матрица:");
+PrintMatrix(firstMartrix);
+
+int[,] secomdMartrix = new int[2, 2];
+MatrixFillArray(secomdMartrix);
+Console.WriteLine("Вторая матрица:");
+PrintMatrix(secomdMartrix);
+
+int[,] resultMatrix = new int[2,2];
+
+SumMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine("Произведение первой и второй матриц:");
+PrintMatrix(resultMatrix);
+
+void SumMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
